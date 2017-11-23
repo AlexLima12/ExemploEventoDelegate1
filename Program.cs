de71188edcf75a5cb2114073ecd72374d4026ed7
ALEX PEREIRA DE LIMA <40344056864@jpmorgan.sp>
2017-11-23 20:30:56 -0200

    fim de exemplo

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ExemploEvento1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro = new Carro(10, "011 654513245");
+            carro.TanqueVazio += new Carro.EventoCarro(taque_vazio);
+            carro.TanqueVazio += new Carro.EventoCarro(ligar_uber);
+            carro.Ligacao += new Carro.Chamar(ligar_casa);
+            carro.Ligar();
+        }
+        static void taque_vazio()
+        {
+            System.Console.WriteLine("Tanque vazio");
+
+        }
+
+        static void ligar_uber(){
+            System.Console.WriteLine("Chamar o Uber...");
+        }
+
+        static void ligar_casa(string telefone){
+            System.Console.WriteLine("Ligando: " + telefone);
+        }
+    }
+}
